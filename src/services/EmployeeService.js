f0cbcfdc9f92fb93ffe0f8d899b891d0d8b5812c
PraneshBalasubramaniam <praneshbk240599@gmail.com>
2021-10-13 12:57:24 +0530

    created student list page

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const Employee_App_Base_Url = "http://localhost:8080/api/v1/employee";
+
+class EmployeeService{
+    
+    getEmployee(){
+        return axios.get(Employee_App_Base_Url);
+    }
+    
+}
+
+export default new EmployeeService();
