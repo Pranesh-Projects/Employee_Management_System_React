ae71d939a8a06d58836398a310efbe54838e2e7e
PraneshBalasubramaniam <praneshbk240599@gmail.com>
2021-10-15 20:20:51 +0530

    created update option

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent";
 
 function App() {
-  return (
-    <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				{/* <div className="container">  -  it puts like a box */}
+					<HeaderComponent />
+					<div className="container">
+						<Switch>
+							<Route path="/" exact component={ListEmployeeComponent}></Route>
+							<Route path="/employees" component={ListEmployeeComponent}></Route>
+							<Route path="/add_employee" component={CreateEmployeeComponent}></Route>
+							<Route path="/update_employee/:id" component={UpdateEmployeeComponent}></Route>
+						</Switch>
+					</div>
+					<FooterComponent />
+				{/* </div> */}
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
-const Employee_App_Base_Url = "http://localhost:8080/api/v1/employee";
+const Employee_App_Base_Url = "http://localhost:8080/api/v1/employees";
 
 class EmployeeService {
 	getEmployee() {
 		return axios.get(Employee_App_Base_Url);
+	}
+
+	createEmployee(employee){
+		return axios.post(Employee_App_Base_Url, employee);
+	}
+
+	getEmployeeById(employeeId){
+		return axios.get(Employee_App_Base_Url + '/' + employeeId);
+	}
+
+	updateEmployee(employee,employeeId){
+		return axios.put(Employee_App_Base_Url + '/' + employeeId, employee);
 	}
 }
 
